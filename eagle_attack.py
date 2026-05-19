5932166a45d1cc94a5f850c543c9c7bf1fa36c61
elmiya0711 <elmiya0711@gmail.com>
2026-05-19 22:24:54 +0700

    update

--- eagle_attack.py
+++ eagle_attack.py
@@ -16,8 +16,7 @@ class EagleAttack:
 
     def banner(self):
     os.system('clear')
-    # ...
-    print(f""" {Fore.RED} .─────────────────────────────────────────────────────────────. {Fore.RED} │{Fore.RED} ██╗ ██╗ ██████╗ ██╗██████╗ ██╗ ██╗██╗ ██████╗ {Fore.RED}│ {Fore.RED} │{Fore.WHITE} ██║ ██║)
+    print(f"""... {Fore.RED} .─────────────────────────────────────────────────────────────. {Fore.RED} │{Fore.RED} ██╗ ██╗ ██████╗ ██╗██████╗ ██╗ ██╗██╗ ██████╗ {Fore.RED}│ {Fore.RED} │{Fore.WHITE} ██║ ██║)
 
     async def attack_engine(self, host, port, path):
         # Asynchronous Zero-Copy Packet Injection
