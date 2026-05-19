2d9f7e126ea85a1ca019d9d5848becc2eb77eaf6
elmiya0711 <elmiya0711@gmail.com>
2026-05-20 00:05:47 +0700

    update

--- eagle-rage.py
+++ eagle-rage.py
@@ -9,7 +9,7 @@ init(autoreset=True)
 
 class EagleAttack:
     def __init__(self):
-‎        self.owner = "MY EAGLE"
+‎        self.owner = "MYEAGLE"
 ‎        self.sent = 0
 ‎        self.start_time = time.time()
 ‎        self.is_running = True
